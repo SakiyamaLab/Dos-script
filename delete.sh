976fcc492856d3ed4929f583f85ff4988466b5bb
Nusk-Rbb <dpanda.kky@gmail.com>
2024-06-25 14:23:28 +0900

    change: delete.sh

--- delete.sh
+++ delete.sh
@@ -1,8 +1,6 @@
 server_ip="192.168.20.226"
 port="12345"
 device=$1
-echo "serial = $client_serial"
-
 "/home/raspberrypi${device}/Develop/lab/db/tsurugi-sql/tgsql-1.3.0/bin/tgsql" -c "tcp://${server_ip}:${port}" --exec "select * from sensorNormal"
 "/home/raspberrypi${device}/Develop/lab/db/tsurugi-sql/tgsql-1.3.0/bin/tgsql" -c "tcp://${server_ip}:${port}" --exec "delete from sensorNormal"
 
