563eb19ddf7f72744355d7adb43663c0e6a1b9b6
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-06-25 14:21:17 +0900

    change: random

--- dos.sh
+++ dos.sh
@@ -5,11 +5,10 @@ arg1=$1
 arg2=$2
 client_ip=$(./get_ip.sh)
 client_serial=$(./serial.sh)
-random_chara=$(./random.sh)
 timestamp=$(./timestamp.sh)
 echo "serial = $client_serial"
 for i in $(seq ${arg1} ${arg2}); do
-	"/home/raspberrypi${device}/Develop/lab/db/tsurugi-sql/tgsql-1.3.0/bin/tgsql" -c "tcp://$server_ip:$port" --exec "insert into sensorNormal(id, hostname, fixedid, payload, ctime) values($i, '$client_ip', '$client_serial', '$random_chara', '$timestamp')"
+	"/home/raspberrypi${device}/Develop/lab/db/tsurugi-sql/tgsql-1.3.0/bin/tgsql" -c "tcp://$server_ip:$port" --exec "insert into sensorNormal(id, hostname, fixedid, payload, ctime) values($i, '$client_ip', '$client_serial', '$(./random.sh)', '$timestamp')"
 	"/home/raspberrypi${device}/Develop/lab/db/tsurugi-sql/tgsql-1.3.0/bin/tgsql" -c "tcp://$server_ip:$port" --exec "insert into sensorNormal(stime) values('$timestamp')"
 done
 
